571234e07355c28a8214a52e30a4e09577ac8f6f
ebagabee <2001.gabrielhas@gmail.com>
2024-11-01 17:25:50 -0300

    refactor(user): using get method to select the path about avatar

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,8 +7,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
 }
