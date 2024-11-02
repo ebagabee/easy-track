08e8ff3cbb2eccfd887b442be52ef2ecda183d2c
ebagabee <2001.gabrielhas@gmail.com>
2024-11-02 16:59:05 -0300

    refactor: updating user to use input decorator to dinamic values about users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './user/dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,5 @@ import { UserComponent } from './user/user.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-fundamentals';
+  users = DUMMY_USERS;
 }
